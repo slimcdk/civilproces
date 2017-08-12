1f220df09f9968c9a320097ee268056d8d1bdc81
Sebastian Andresen <sebastian.sommersted@live.dk>
2017-08-12 14:42:31 +0200

    Added routes to subpages

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,42 @@ router.get('/users/login', function(req, res){
 });
 
 
+// Get event 1
+router.get('/event_1', function (req, res) {
+    res.render('event_1', {title: "Event 1"});
+});
+
+
+// Get event 2
+router.get('/event_2', function (req, res) {
+    res.render('event_2', {title: "Event 2"});
+});
+
+
+// Get event 3
+router.get('/event_3', function (req, res) {
+    res.render('event_3', {title: "Event 3"});
+});
+
+
+// Get blog
+router.get('/blog', function (req, res) {
+    res.render('blog', {title: "Blog"});
+});
+
+
+// Get about
+router.get('/about', function (req, res) {
+    res.render('about', {title: "About"});
+});
+
+
+// Get login
+/*router.get('/login', function (req, res) {
+    res.render('login', {title: "Login"});
+});*/
+
+
 function ensureAuthenticated(req, res, next){
 	if(req.isAuthenticated()){
 		return next();
