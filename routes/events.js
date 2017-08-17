f9570291eadc15b012f90c9add901f3b31265b59
Sebastian Andresen <sebastian.sommersted@live.dk>
2017-08-18 00:39:35 +0200

    Descriptions added to each page

--- routes/events.js
+++ routes/events.js
@@ -1,17 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/*
-// Get article 01
-router.get('/01', function (req, res) {
-    res.render('blog/article_01');
-});
-
-// Get article 02
-router.get('/02', function (req, res) {
-    res.render('blog/article_02');
-});*/
-
 
 // Get event 1
 router.get('/event_1', function (req, res) {
