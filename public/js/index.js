3f7681c8955b9bd7bb9de2b6f19b9d1f97cf8bcb
Sebastian Andresen <sebastian.sommersted@live.dk>
2017-08-24 17:07:26 +0200

    Bootstrap styling needs fix

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,6 @@
 /**
  * Created by ste on 24-08-2017.
  */
-/*$.get('/event:1').then(function(responseData) {
- //responseData is content from other page.
- //$('#someElem').append(responseData);
- //console.log(responseData);
- console.log(template_to_json(responseData));
- });*/
 
 getData("length", function(response){
     var events = [];
@@ -40,10 +34,28 @@ function readPage(id, handleData) {
 }
 
 function eventsReady(events) {
-    console.log(events);
+    events = sortEvents(events);
+
+    $( "#event_menu" ).append('<ul id="event_menu_list"></ul>');
+
     for (var i = 0; i < events.length; i++) {
         console.log(events[i]);
         $( "#event_menu_list" ).append("<li><a href='/event:" + events[i].index + "'>" + events[i].title + "</a></li>");
     }
 }
 
+function sortEvents(events) {
+    var result = [];
+
+    for (var i = 1; i <= events.length; i++) {
+        for (var j = 0; j < events.length; j++) {
+            if (events[j].index == i) {
+                result.push(events[j]);
+            }
+        }
+    }
+
+    return result;
+}
+
+$("#body_box, #top_menu, #info, #test").removeClass("col-xs-12").addClass("col-xs-9");
