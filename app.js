fc7111d29f51f689452483cdebb4ffaebfd32ece
Christian <cs@slimc.dk>
2017-08-29 17:57:10 +0200

    Added public ip range

--- app.js
+++ app.js
@@ -89,6 +89,6 @@ app.use("*", function (req, res) {
 // Set Port
 app.set('port', (process.env.PORT || 3000));
 
-app.listen(app.get('port'), function(){
+app.listen(app.get('port'), "0.0.0.0", function(){
 	console.log('Server started on port '+app.get('port'));
 });
