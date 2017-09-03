aea3dfc2bb3a9036ecd67bbb8708a307d83aa5e8
Sebastian Andresen <sebastian.sommersted@live.dk>
2017-09-03 04:52:40 +0200

    event-list scroll fix

--- public/js/index.js
+++ public/js/index.js
@@ -71,52 +71,7 @@ function eventsReady(events) {
         $("#index-event-box-bottom").append("<div class='event-btn-box col-xs-12 col-sm-6'>" + box + "</div>");
     }
 
-
-
-
-
-
-
     $("#index-event-box-right").addClass("onPage");
+    $("#index-event-box-bottom").addClass("onPage");
 }
 
-
-
-
-
-
-
-/*
-function eventsReady(events) {
-    events = sortEvents(events);
-
-    $( "#front_event_menu" ).append('<table id="event_menu_list" border="1"></table>');
-
-    for (var i = 0; i < events.length; i++) {
-        var box = "";
-        var event = events[i];
-        console.log(event);
-
-        box += "<div class='front_event_box_shell'>"; //dedicated button space
-            box += "<a href='/event:" + event.index + "'>";
-                box += "<div class='front_event_box'>"; //the actual button
-                    box += "<div class='front_event_text_box'>"; //text box
-
-                        box += "<p>" + event.title + "</p>";
-                        box += "<p>" + event.time + "</p>";
-                        box += "<p>" + event.price + "</p>";
-
-                    box += "</div>";
-                box += "</div>";
-            box += "</a>";
-        box += "</div>";
-
-        $( "#event_menu_list" ).append("<tr><td>" + box + "</td></tr>");
-    }
-}
-*/
-/*
-$(document).ready(function() {
-    $("#body_box, #top_menu, #info").removeClass("col-md-12").addClass("col-md-9");
-    //$("#front_event_menu").removeClass("invisible");
-});*/
